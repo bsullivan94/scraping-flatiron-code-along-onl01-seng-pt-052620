b196bb5b555de6f87e1c6832876b3b198326c68f
brody sullivan <brodysullivan0@gmail.com>
2020-06-30 14:34:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+
 require_relative './course.rb'
 
 class Scraper
@@ -10,6 +11,7 @@ class Scraper
     
     binding.pry
   end
+
 end
 
 Scraper.new.get_page
